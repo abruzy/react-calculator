1ee867771aac36dd25f4ba299381fd6125040d00
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-06-26 17:58:18 +0100

    change the color from white to black for the operator button

--- src/components/button-panel/button-panel.component.jsx
+++ src/components/button-panel/button-panel.component.jsx
@@ -7,8 +7,7 @@ const isOperator = val => val === '+' || val === '-' || val === '*' || val === '
 const isZero = val => val === '0';
 
 const ButtonPanel = props => (
-  <div
-    className={`button-panel ${isOperator(props.children) ? 'operator' : null} ${isZero(props.children) ? 'span' : null}`}>{props.children}
+  <div className={`button-panel ${isOperator(props.children) ? 'operator' : null} ${isZero(props.children) ? 'span' : null}`}>{props.children}
   </div>
 );
 
