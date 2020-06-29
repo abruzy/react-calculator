4cfd561c9ec36e3694f47490e1c3ded40946b0db
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-06-29 17:04:38 +0100

    added calculate to App.js and change my App component from functional component to state component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
 
+import calculate from '../logic/calculate';
+
 import '../styles/scss/App.scss';
 
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="calc-wrapper">
-        <Display>0</Display>
-        <ButtonPanel />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: '0',
+      next: '0',
+      operation: '',
+      result: '',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="calc-wrapper">
+          <Display>0</Display>
+          <ButtonPanel />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
