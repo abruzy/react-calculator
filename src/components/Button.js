fca34a25a10f7ae0c2fe60bd00f189766e9621b0
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-06-29 21:07:21 +0100

    set default color and wide props

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,18 +4,23 @@ import PropTypes from 'prop-types';
 import '../styles/scss/Button.scss';
 
 const Button = ({ name, color, wide }) => {
-  const isOperator = color ? 'operator' : 'button';
-  const isWide = wide ? 'span' : '';
+  // const isOperator = color ? 'operator' : 'button';
+  // const isWide = wide ? 'span' : '';
 
   return (
-    <div type="button" className={`button ${isOperator} ${isWide}`}>{name}</div>
+    <div type="button" className="button" style={{ backgroundColor: color ? '#f5913e' : '', flex: wide ? '25%' : '' }}>{name}</div>
   );
+};
+
+Button.defaultProps = {
+  wide: false,
+  color: '#f5913e',
 };
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
-  color: PropTypes.bool.isRequired,
-  wide: PropTypes.bool.isRequired,
+  wide: PropTypes.bool,
+  color: PropTypes.string,
 };
 
 export default Button;
