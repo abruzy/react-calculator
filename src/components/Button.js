67e90290193cc276ed0fdcdcf5753d8c9333192c
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-06-29 21:10:02 +0100

    fix: linters error

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,14 +3,9 @@ import PropTypes from 'prop-types';
 
 import '../styles/scss/Button.scss';
 
-const Button = ({ name, color, wide }) => {
-  // const isOperator = color ? 'operator' : 'button';
-  // const isWide = wide ? 'span' : '';
-
-  return (
-    <div type="button" className="button" style={{ backgroundColor: color ? '#f5913e' : '', flex: wide ? '25%' : '' }}>{name}</div>
-  );
-};
+const Button = ({ name, color, wide }) => (
+  <div type="button" className="button" style={{ backgroundColor: color ? '#f5913e' : '', flex: wide ? '25%' : '' }}>{name}</div>
+);
 
 Button.defaultProps = {
   wide: false,
